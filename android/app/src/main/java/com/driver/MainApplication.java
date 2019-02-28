3e56e2e953992b070f57099bcf66943283504a09
Yingi Kem <yingikeme@yahoo.com>
2019-02-28 03:48:09 -0800

    Working Notification

--- android/app/src/main/java/com/driver/MainApplication.java
+++ android/app/src/main/java/com/driver/MainApplication.java
@@ -3,7 +3,6 @@ package com.driver;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -35,7 +34,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage(),
             new VectorIconsPackage(),
             new RNFirebasePackage(),
             new RNFirebaseAuthPackage(),
